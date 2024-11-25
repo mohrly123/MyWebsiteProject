a1abe0feda09845aa7553317f03d4bd5527573f9
mohrly123 <168989632+mohrly123@users.noreply.github.com>
2024-11-25 11:58:40 +0100

    .
    .

---
+++ heberechner/rechnerHebung/rechnerHebung.js
@@ -0,0 +1,22 @@
+let sollHoehe = 0;
+
+let istHoehe = 0;
+
+let aWert = 0;
+
+document.getElementById("inputSoll").addEventListener("input", (event) => {
+    sollHoehe = event.target.value;
+    
+});
+
+document.getElementById("inputIst").addEventListener("input", (event) => {
+    istHoehe = event.target.value;
+   
+});
+
+if (sollHoehe !== 0){
+    let inputAWert = document.getElementById("inputAWert")
+    
+}
+
+
