7a4c6e788ea370741a3cb8d02469d236894ad1c6
mohrly123 <168989632+mohrly123@users.noreply.github.com>
2024-11-25 10:34:33 +0100

    .
    .

--- heberechner/hebung.js
+++ heberechner/hebung.js
@@ -1,3 +1,13 @@
 document.getElementById("closeBtn").addEventListener("click", () => {
     location.href = "../index.html";
 })
+
+// Von der HebungsMainPage zum Hebungsrechner
+document.getElementById("hebungsrechner").addEventListener("click", () => {
+    location.href = "./rechnerHebung/rechnerHebung.html";
+})
+
+// Von der HebungsMainPage zum Hebungsrechner
+document.getElementById("verschiebungsrechner").addEventListener("click", () => {
+    location.href = "./rechnerVerschiebung/rechnerVerschiebung.html";
+})
